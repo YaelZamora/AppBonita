f5818bdcce764b31844e57f9026d9bfe874ea1b0
Yael Zamora <ajedrezkasparov@gmail.com>
2024-10-04 14:42:53 -0600

    Initial Commit

---
+++ AppBonita/AppBonitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppBonitaApp.swift
+//  AppBonita
+//
+//  Created by Yael Javier Zamora Moreno on 04/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AppBonitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
